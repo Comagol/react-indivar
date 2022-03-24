3bf3e90d2cd98dd1a5a416cabadf2896ad604709
Francisco Comabella <comabellafrancisco@gmail.com>
2022-03-24 16:47:09 -0300

    agrego elNavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style= {{color: 'purple', backgroundColor: 'blue'}}>
+          Indivar Deco
         </p>
         <a
           className="App-link"
---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import'./NavBar.css'
+
+const NavBar = () => {
+    return(
+        <nav className="nav">
+            <ul className="navUl">
+                <li className="navUlLi">
+                    <a href="" className="navUlLiA">Indivar Deco</a>    
+                </li>
+                <li className="navUlLi">
+                    <a href="" className="navUlLiA">Productos</a>    
+                </li>
+                <li className="navUlLi">
+                    <a href="" className="navUlLiA">Nosotros</a>    
+                </li>
+                <li className="navUlLi">
+                    <a href="" className="navUlLiA">Contacto</a>    
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
